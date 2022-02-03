a9f70eb8cc4ae1e0895b89aeb28bd3b8e3fc690b
unknown <dortizchaves@outlook.com>
2022-02-03 17:10:46 -0600

    created unit and integration tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,12 +1,45 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
-RSpec.describe 'Creating a book', type: :feature do
+RSpec.describe 'Life cyle of a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J.K. Rowling'
+    fill_in 'Date', with: '02/22/2022'
+    fill_in 'Price', with: 13.49
     click_on 'Create Book'
+
     visit books_path
     expect(page).to have_content('harry potter')
+
+    visit show_book_path
+    expect(page).to have_content('harry potter')
+    expect(page).to have_content('J.K. Rowling')
+    expect(page).to have_content(13.49)
+    expect(page).to have_content('02/22/2022')
+
+    visit edit_book_path
+    expect(page).to have_content('harry potter')
+    expect(page).to have_content('J.K. Rowling')
+    expect(page).to have_content(13.49)
+    expect(page).to have_content('02/22/2022')
+    fill_in 'Title', with: 'harry potter 10: The secrets we never knew'
+    click_on 'Update'
+    expect(page).to have_content('harry potter 10: The secrets we never knew')
+
+    visit books_path
+    expect(page).to have_content('harry potter 10: The secrets we never knew')
+
+    click_on 'Destroy'
+    expect(page).to have_content('Are you sure?')
+    click_on 'OK'
+    expect(page).to have_no_content('harry potter 10: The secrets we never knew')
+  end
+
+  scenario 'blank inputs' do
+    visit new_book_path
+    click_on 'Create Book'
+    expect(page).to have_content('Title can\'t be blank')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,15 +3,26 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
-  end
+    described_class.new(title: 'harry potter','02/22/2022', 'J.K. Rowling',13.49)
+  end/
 
-  it 'is valid with valid attributes' do
+  it '/is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
+  it 'is not valid without values' do
     subject.title = nil
+    subject.date = nil
+    subject.author = nil
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid with bad values' do
+    subject.title = 13.49
+    subject.date = 'harry potter'
+    subject.author = '2/22/2022'
+    subject.price = 'J.K. Rowling'
     expect(subject).not_to be_valid
   end
 end
