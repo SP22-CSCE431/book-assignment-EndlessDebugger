bbb3ad2b65d0d0dd72e916d13dffda11b0d9bb4d
Daniel Ortiz-Chaves <dortizchaves@outlook.com>
2022-02-03 19:20:24 -0600

    remove merge head

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
   resources :books
-<<<<<<< HEAD
   root 'books#index'
 end
-=======
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
->>>>>>> main
